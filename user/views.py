349b4513175aaebb13699aff555f01f39eee1649
nikitabolkar123 <bolkarnikita2001@gmail.com>
2023-03-14 15:58:02 +0530

    [Nikita] Added. User Registration in Book Store App

--- user/views.py
+++ user/views.py
@@ -1,6 +1,8 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from logconfig.logger import get_logger
+from user.models import User
 from user.serializers import RegistrationSerializer
 
 logger = get_logger()
@@ -22,7 +24,20 @@ class UserRegistration(APIView):
             serializer.is_valid(raise_exception=True)
             serializer.save()
             return Response({"message": "User Registration Successfully", "status": 201, "data": serializer.data},
-                            status=201)
+                            status=status.HTTP_201_CREATED)
         except Exception as e:
             logger.exception(e)
-            return Response({"message": str(e), "status": 400, "data": {}}, status=400)
+            return Response({"message": str(e), "status": 400, "data": {}}, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request):
+        """
+            this method is used to retrieve the all registered users data
+        """
+        try:
+            user = User.objects.all()
+            serializer = RegistrationSerializer(user, many=True)
+            return Response({"message": "Retrieve Data  Successfully", "status": 200, "data": serializer.data},
+                            status=status.HTTP_200)
+        except Exception as e:
+            logger.exception(e)
+        return Response({"message": str(e), "status": 400, "data": {}}, status=status.HTTP_400_BAD_REQUEST)
